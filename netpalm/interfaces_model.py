23abc62a6a799c219182ab5bece50de3fd27b373
ndo@$(hostname) <yo@ndo.dev>
2020-08-17 08:56:37 +0200

    update(netpalm): basic changes for interface

--- netpalm/interfaces_model.py
+++ netpalm/interfaces_model.py
@@ -4,23 +4,27 @@ from typing import Dict, FrozenSet, List, Optional, Sequence, Set, Tuple, Union
 from backend.core.models.service import service_lifecycle
 from backend.core.models.models import queue_strat
 
-class boilerplate_model_args(BaseModel):
+class interface_model_args(BaseModel):
     # your model goes here!
     hosts: list
+    username: str
+    password: str
 
-class boilerplate_model(BaseModel):
+class interface_model(BaseModel):
     # this class MUST match the filename & the filename must be formatted $servicetemplatename_model.py
     operation: service_lifecycle
-    args: boilerplate_model_args
+    args: interface_model_args
     queue_strategy: Optional[queue_strat] = None
 
     class Config:
         # add an example payload under the "example" dict 
         schema_extra = {
             "example": {
-                "operation": "create",
+                "operation": "retrieve",
                 "args":{
                     "hosts":["10.0.2.25","10.0.2.23"],
+                    "username": "{{device_username}}",
+                    "password": "{{device_password}}",
                 }
             }
         }
